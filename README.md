7787b802e3db47b572a6cc5f2614746bc8cf403a
ROODY <rody9672@gmail.com>
2022-07-16 23:35:11 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Soquet-Server
+
+
+`Reconstruir modulos de NOde
+`````````````````
+npm install
+``````````````
+
+generar el Dist
+
+tsc -w
+
+````````````````
+
+Levantar servider,cualquiera de estos dos comandos
+nodemon dist/
+node dist/
---
+++ index.ts
@@ -0,0 +1,23 @@
+import Server from "./clases/server";
+import  router  from "./routes/router";
+import bodyParser  from "body-parser";
+import cors from 'cors';
+/**/ 
+
+
+const server = new Server();
+
+//Body parser
+server.app.use(bodyParser.urlencoded({extended: true}))
+server.app.use(bodyParser.json());
+
+//cors
+server.app.use( cors ({ origin: true, credentials: true}));
+
+server.app.use('/',router);
+
+
+ server.start( () =>
+ {
+     console.log(`servidor corriendo en el puerto ${server.port}`)
+ });
